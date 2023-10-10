19ecc367900a257568f0a516a6d9f90e32eb5912
Thiagopha12k <111284868+Thiagopha12k@users.noreply.github.com>
2023-10-10 09:32:48 -0300

    Add files via upload

--- main-main/main-main/main.js
+++ main-main/main-main/main.js
@@ -1,8 +1,21 @@
-// Pom ----------------------------------------
-function tocaSomPom (){
-    document.querySelector('#som_tecla_pom').play();
+// Pom -------------------------
+function TocaSomPom (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('tecla');
+const listaDeteclas = document.querySelectorAll('.tecla');
+ 
 
-listaDeTeclas[0].onclick = tocaSomPom
+//para
+for (let contador = 0; contador < listaDeteclas.length; contador++) {
+
+    const tecla = listaDeteclas [contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`//template string
+
+
+    teclado.onclick = function (){
+        tocaSom(idAudio);
+    }
+
+}
--- main-main/main.js
+++ main-main/main.js
@@ -1,21 +1,30 @@
-// Pom -------------------------
-function TocaSomPom (idElementoAudio){
+function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
-const listaDeteclas = document.querySelectorAll('.tecla');
- 
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //para
-for (let contador = 0; contador < listaDeteclas.length; contador++) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-    const tecla = listaDeteclas [contador];
+    const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`//template string
+    const idAudio = `#som_${instrumento}`; //template string
 
-
-    teclado.onclick = function (){
+    tecla.onclick = function () {
         tocaSom(idAudio);
     }
+
+    tecla.onkeyown = function (evento){
+        console.log(evento.code  == 'Space') 
+        if(evento.code === 'Space'){
+            tecla.classList.add('ativa') ;
+        }
+    }
+
+    tecla.onkeyup  = function () {
+        tecla.classList.remove('ativa');
+    }
+
 
 }
